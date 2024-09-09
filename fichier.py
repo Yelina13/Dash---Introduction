01262839ee679140351a374680a066be1198fbb7
Yelina Taibou Diallo <ytaibou.diallo@gmail.com>
2024-09-09 09:55:53 +0200

    second dash

--- fichier.py
+++ fichier.py
@@ -1,41 +1,61 @@
-from dash import Dash, html, dcc, Input, Output
-import plotly.express as px
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
+import plotly.express as px
 
-# Création de l'application
+# Incorporer les données
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialiser l'application
 app = Dash(__name__)
 
-# Exemple de données
-df = pd.DataFrame({
-    'x': [1, 2, 3, 4, 5],
-    'y': [10, 11, 12, 13, 14]
-})
-
-# Définition du layout avec un Slider
+# Mise en page de l'application
 app.layout = html.Div([
-    html.H1("Dashboard avec Dash"),
-    dcc.Graph(id='line-graph'),
+    html.H1("Tableau de bord avec Dash"),
+    html.Div(children='Ma première application avec des données, un graphique et des contrôles'),
+    html.Hr(),
+    dcc.RadioItems(
+        options=['pop', 'lifeExp', 'gdpPercap'], 
+        value='lifeExp', 
+        id='controls-and-radio-item'
+    ),
+    dash_table.DataTable(
+        data=df.to_dict('records'), 
+        page_size=6
+    ),
+    dcc.Graph(id='controls-and-graph'),
     dcc.Slider(
         id='slider',
-        min=df['x'].min(),
-        max=df['x'].max(),
+        min=1,
+        max=5,
         value=2,  # valeur initiale
-        marks={str(num): str(num) for num in df['x']},
+        marks={str(num): str(num) for num in range(1, 6)},
         step=None
-    )
+    ),
+    dcc.Graph(id='line-graph')
 ])
 
-# Callback pour mettre à jour le graphique
-@app.callback(
-    Output('line-graph', 'figure'),
-    [Input('slider', 'value')]
+# Callback pour mettre à jour le premier graphique en fonction du bouton radio
+@callback(
+    Output('controls-and-graph', 'figure'),
+    Input('controls-and-radio-item', 'value')
 )
-def update_graph(slider_value):
-    # Filtrer les données en fonction du curseur
-    filtered_df = df[df['x'] <= slider_value]
+def update_histogram(col_chosen):
+    fig = px.histogram(df, x='continent', y=col_chosen, histfunc='avg')
+    return fig
+
+# Callback pour mettre à jour le graphique en ligne en fonction de la valeur du slider
+@callback(
+    Output('line-graph', 'figure'),
+    Input('slider', 'value')
+)
+def update_line_graph(slider_value):
+    # Exemple de données pour le graphique en ligne
+    data = {'x': [1, 2, 3, 4, 5], 'y': [10, 11, 12, 13, 14]}
+    df_line = pd.DataFrame(data)
+    filtered_df = df_line[df_line['x'] <= slider_value]
     fig = px.line(filtered_df, x='x', y='y', title='Graphique Simple')
     return fig
 
-# Exécution de l'application
+# Exécuter l'application
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
